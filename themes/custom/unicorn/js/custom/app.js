b458dee6ec946a4ae393c936394f97819af8f473
sebsebseb123 <sebastian.lesch@gmail.com>
2014-02-06 14:38:44 -0500

    WIP

---
+++ themes/custom/unicorn/js/custom/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('ufApp', [
+  'getter.directives',
+  'LocalStorageModule',
+  'ngSanitize',
+  'ngRoute'
+]);
--- themes/custom/unicorn/template.php
+++ themes/custom/unicorn/template.php
@@ -4,3 +4,25 @@
  * @file
  * template.php
  */
+
+/**
+ * Implements hook_preprocess_block().
+ */
+function unicorn_preprocess_block(&$vars) {
+  // Check for our blocks, and add proper ng-controller directives.
+  switch ($vars['block']->delta) {
+    case 'uf_projects_list':
+      $vars['attributes_array']['ng-controller'] = 'ProjectsCtrl';
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function unicorn_preprocess_html(&$vars) {
+  // Add the ng-app directive to the body tag.
+  $vars['attributes_array']['ng-app'] = 'ufApp';
+}
