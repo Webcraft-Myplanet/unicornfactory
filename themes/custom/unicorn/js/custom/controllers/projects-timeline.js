f5f02d62e610581afb8ac1767b760811924f93d5
sjagr <s.jager@gmail.com>
2014-02-11 13:55:10 -0500

    Wrote a visual display algorithm for project-timelines controller

--- themes/custom/unicorn/js/custom/controllers/projects-timeline.js
+++ themes/custom/unicorn/js/custom/controllers/projects-timeline.js
@@ -17,14 +17,33 @@ angular.module('ufApp')
         var pageData = {};
         pageData.projects = data;
 
-        /*var index;
+        // Set up a timestamp for right now
+        var currTime = Date.now();
+
+        // Set up the number of days in each month
+        var daysInMonth = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+
+        var bootColToTime = new Array();
+        var index;
+        var colIndex = 0;
+        for (index = currTime.getMonth(); index < (currTime.getMonth() + 4); ++index) {
+          bootColToTime[3 * index] = currTime + (daysInMonth[index] * 86400 / 3);
+          bootColToTime[3 * index + 1] = currTime + (daysInMonth[index] * 86400 * 2 / 3);
+          bootColToTime[3 * index + 2] = currTime + (daysInMonth[index] * 86400);
+        }
+        console.log(bootColToTime[0]);
+        console.log("Hello!");
+        // Set up some temp vars
+        var startDiff, bootOffset, bootSize;
+
         for (index = 0; index < pageData.projects.length; ++index) {
-          var startDate = new Date(pageData.projects[index].projectStartDate);
-          var endDate = new Date(pageData.projects[index].projectEndDate);
-          //pageData.projects[index][projectStartDateObj] = startDate;
-          //pageData.projects[index][projectEndDateObj] = endDate;
+          // Convert each projectStartDate and projectEndDate into a usable Date object
+          pageData.projects[index].projectStartDateObj = new Date(pageData.projects[index].projectStartDate);
+          pageData.projects[index].projectEndDateObj = new Date(pageData.projects[index].projectEndDate);
           //console.log(pageData.projects[index]);
-        } */
+          // Determine the Bootstrap offset to visually represent the starting date in the Timeline
+          //startDiff = pageData.projects[index].projectStartDateObj.getTime() - currTime;
+        }
 
         // Then return it.
         return pageData;
