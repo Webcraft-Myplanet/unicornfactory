7709453249cf8d6b93721b486a40f1ff2276e1ca
sjagr <s.jager@gmail.com>
2014-02-21 12:08:30 -0500

    More progress yayyyy

--- themes/custom/unicorn/templates/user-profile.tpl.php
+++ themes/custom/unicorn/templates/user-profile.tpl.php
@@ -1,21 +1,18 @@
-<section id="wrapper" class="fluid-container">
+<section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="wrapper" class="fluid-container">
 
   <!-- personal info div containing avatar, name and current team status -->
-  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>">
+  <section>
     <div class="row">
 
       <div class="personal_avatar col-lg-4">
-        <div ng-bind-html="page.Avatar" class"img-thumbnail" alt=""></div>
+        <div ng-bind-html="page.field_avatar.und.0.html" class"img-thumbnail" alt=""></div>
       </div>
       <div class="name_status col-lg-4">
-        <h2>{{page.users_name}}</h2>
+        <h2>{{page.name}}</h2>
         <p>{{page.users_mail}}</p>
         <!-- The following ng-show/ng-hide depend on value of Slogan.
           -- if Slogan is defined it will display, otherwise a link to add a slogan. -->
-        <div ng-show="page.Slogan"><p>{{page.Slogan}}</p></div>
-        <div ng-hide="page.Slogan">
-          <a href="/user/{{page.uid}}/edit">Add a Slogan</a>
-        </div>
+        <div><p onaftersave="updateUser()" editable-text="page.field_slogan.und.0.value">{{page.field_slogan.und.0.value || 'Add a slogan...' }}</p></div>
         <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.TeamID[0]}}">{{page.TeamName[0]}}</a></button>
         <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.ProjectID[0]}}">{{page.ProjectName[0]}}</a></button>
       </div>
@@ -33,7 +30,7 @@
   <hr>
 
   <!-- dynamic skills section with accordion fold -->
-  <section ng-controller="UserSkillsCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="skills" class="container-fluid">
+  <section id="skills" class="container-fluid">
     <!-- static header -->
     <div class="row common_title">
       <div id="skills_header" class="row">
@@ -62,7 +59,7 @@
 
   <hr>
   <!-- static project section with dynamic project inputs -->
-  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="projects" class="container-fluid">
+  <section id="projects" class="container-fluid">
     <div class="project_header common_title">
       <h2>Projects</h2>
     </div>
