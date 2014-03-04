c2f2660858bd6110265bc98d8a1cf031dd534f66
bhawkeswood <bhawkeswood@gmail.com>
2014-03-04 10:15:57 -0500

    Updating profile template and eliminate old version

--- themes/custom/unicorn/templates/user-profile.tpl.php
+++ themes/custom/unicorn/templates/user-profile.tpl.php
@@ -1,28 +1,27 @@
-<section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="wrapper" class="fluid-container">
+<section id="wrapper" class="fluid-container">
 
   <!-- personal info div containing avatar, name and current team status -->
-  <section>
+  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?> ">
     <div class="row">
-
       <div class="personal_avatar col-lg-4">
-        <div ng-bind-html="page.field_avatar.und.0.html" class"img-thumbnail" alt=""></div>
+        <!-- For some reason the avatar is loading, but being returned a 403 Forbidden error -->
+        <img ng-src="{{avatar}}"/>
       </div>
       <div class="name_status col-lg-4">
-        <h2>{{page.name}}</h2>
-        <!-- We need to capture the user's password in order to change their e-mail address... -->
-        <p onaftersave="updateUser()" editable-text="page.mail">{{page.mail}}</p>
+        <h2 editable-text="page.name" buttons="no" onbeforesave="validateName($data)" onaftersave="updateUser()" e-form="nameEdit" ng-click="nameEdit.$show()">{{page.name}}</h2>
+        <p>{{page.mail}}</p>
         <!-- The following ng-show/ng-hide depend on value of Slogan.
-          -- if Slogan is defined it will display, otherwise a link to add a slogan. -->
-        <div><p onaftersave="updateUser()" editable-text="page.field_slogan.und.0.value">{{page.field_slogan.und.0.value || 'Add a slogan...' }}</p></div>
-        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.TeamID[0]}}">{{page.TeamName[0]}}</a></button>
-        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.ProjectID[0]}}">{{page.ProjectName[0]}}</a></button>
+           if Slogan is defined it will display, otherwise a link to add a slogan. -->
+        <div editable-text="page.field_slogan.und[0].value" buttons="no" onbeforesave="validateSlogan($data)" onaftersave="updateUser()" e-form="sloganEdit" ng-click="sloganEdit.$show()"><p>{{page.field_slogan.und[0].value}}</p></div>
+        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.related_teams[0].nid}}">{{page.related_teams[0].name}}</a></button>
+        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.related_projects[0].nid}}">{{page.related_projects[0].name}}</a></button>
       </div>
       <div class="personal_social col-lg-4">
         <ul class="social_network row">
-          <li class="col-lg-3"><a href="https://www.facebook.com/{{ page.field_facebook.und[0].value }}" class="btn btn-default btn-lg btn-primary active" role="button">FB</a></li>
-          <li class="col-lg-3"><a href="https://twitter.com/{{ page.field_twitter.und[0].value }}" class="btn btn-default btn-lg" role="button">TW</a></li>
-          <li class="col-lg-3"><a href="http://github.com/{{ page.field_github.und[0].value }}" class="btn btn-default btn-lg" role="button">GH</a></li>
-          <li class="col-lg-3"><a href="{{ page.field_linkedin.und[0].value }}" class="btn btn-default btn-lg" role="button">LI</a></li>
+          <li class="col-lg-3"><a href="https://www.facebook.com/{{page.field_facebook.und[0].value}}" class="btn btn-default btn-lg btn-primary active" role="button">FB</a></li>
+          <li class="col-lg-3"><a href="https://twitter.com/{{page.field_twitter.und[0].value}}" class="btn btn-default btn-lg" role="button">TW</a></li>
+          <li class="col-lg-3"><a href="http://github.com/{{page.field_github.und[0].value}}" class="btn btn-default btn-lg" role="button">GH</a></li>
+          <li class="col-lg-3"><a href="{{page.field_linkedin.und[0].url}}" class="btn btn-default btn-lg" role="button">LI</a></li>
         </ul>
       </div>
     </div>
@@ -31,7 +30,7 @@
   <hr>
 
   <!-- dynamic skills section with accordion fold -->
-  <section id="skills" class="container-fluid">
+  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="skills" class="container-fluid">
     <!-- static header -->
     <div class="row common_title">
       <div id="skills_header" class="row">
@@ -60,16 +59,17 @@
 
   <hr>
   <!-- static project section with dynamic project inputs -->
-  <section id="projects" class="container-fluid">
+  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="projects" class="container-fluid">
     <div class="project_header common_title">
       <h2>Projects</h2>
     </div>
 
     <hr>
 
-    <div class="project_content row">
-      <div class="col-lg-4" ng-repeat="project in page.related_projects">
-        <h3><a href="/node/{{project.nid}}">{{project.name}}</a></h3>
+    <div class="project_content row" ng-repeat="project in page.related_projects">
+      <div class="project1 col-lg-4" >
+        <h3><a href="node/{{project.nid}}">{{project.name}}</a></h3>
+        <div ng-bind-html="project.avatar"></div>
       </div>
     </div>
   </section>
