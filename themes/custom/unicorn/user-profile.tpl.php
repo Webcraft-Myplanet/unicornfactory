9f20614a3ed72103da25d1ac822f00b41b5e349b
bhawkeswood <bhawkeswood@gmail.com>
2014-03-04 14:09:53 -0500

    Cleaning up projects section at bottom of user-profile

--- themes/custom/unicorn/user-profile.tpl.php
+++ themes/custom/unicorn/user-profile.tpl.php
@@ -1,29 +1,27 @@
 <section id="wrapper" class="fluid-container">
 
   <!-- personal info div containing avatar, name and current team status -->
-  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>">
+  <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?> ">
     <div class="row">
       <div class="personal_avatar col-lg-4">
-        <div ng-bind-html="page.Avatar" class"img-thumbnail" alt=""></div>
+        <!-- For some reason the avatar is loading, but being returned a 403 Forbidden error -->
+        <img ng-src="{{avatar}}"/>
       </div>
       <div class="name_status col-lg-4">
-        <h2>{{page.name}}</h2>
+        <h2 editable-text="page.name" buttons="no" onbeforesave="validateName($data)" onaftersave="updateUser()" e-form="nameEdit" ng-click="nameEdit.$show()">{{page.name}}</h2>
         <p>{{page.mail}}</p>
         <!-- The following ng-show/ng-hide depend on value of Slogan.
            if Slogan is defined it will display, otherwise a link to add a slogan. -->
-        <div ng-show="page.Slogan"><p>{{page.Slogan}}</p></div>
-        <div ng-hide="page.Slogan">
-          <a href="/user/{{page.uid}}/edit">Add a Slogan</a>
-        </div>
-        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.TeamID[0]}}">{{page.TeamName[0]}}</a></button>
-        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.ProjectID[0]}}">{{page.ProjectName[0]}}</a></button>
+        <div editable-text="page.field_slogan.und[0].value" buttons="no" onbeforesave="validateSlogan($data)" onaftersave="updateUser()" e-form="sloganEdit" ng-click="sloganEdit.$show()"><p>{{page.field_slogan.und[0].value}}</p></div>
+        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.related_teams[0].nid}}">{{page.related_teams[0].name}}</a></button>
+        <button type="button" class="btn btn-default col-lg-8"><a href="/node/{{page.related_projects[0].nid}}">{{page.related_projects[0].name}}</a></button>
       </div>
       <div class="personal_social col-lg-4">
         <ul class="social_network row">
-          <li class="col-lg-3"><a href="https://www.facebook.com/{{page.Facebook}}" class="btn btn-default btn-lg btn-primary active" role="button">FB</a></li>
-          <li class="col-lg-3"><a href="https://twitter.com/{{page.Twitter}}" class="btn btn-default btn-lg" role="button">TW</a></li>
-          <li class="col-lg-3"><a href="http://github.com/{{page.Github}}" class="btn btn-default btn-lg" role="button">GH</a></li>
-          <li class="col-lg-3"><a href="{{page.Linkedin}}" class="btn btn-default btn-lg" role="button">LI</a></li>
+          <li class="col-lg-3"><a href="https://www.facebook.com/{{page.field_facebook.und[0].value}}" class="btn btn-default btn-lg btn-primary active" role="button">FB</a></li>
+          <li class="col-lg-3"><a href="https://twitter.com/{{page.field_twitter.und[0].value}}" class="btn btn-default btn-lg" role="button">TW</a></li>
+          <li class="col-lg-3"><a href="http://github.com/{{page.field_github.und[0].value}}" class="btn btn-default btn-lg" role="button">GH</a></li>
+          <li class="col-lg-3"><a href="{{page.field_linkedin.und[0].url}}" class="btn btn-default btn-lg" role="button">LI</a></li>
         </ul>
       </div>
     </div>
@@ -46,8 +44,9 @@
     </div>
 
     <hr>
-    <!-- accordion content for skills -->
-    <div class="progress" ng-repeat="skill in skills | orderBy:'-current'">
+
+    <!-- top 3 skills -->
+    <div class="progress" ng-repeat="skill in skills | orderBy:'-current'| limitTo:3 ">
       <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%">
         <span>Current {{skill.name}} level</span>
       </div>
@@ -57,9 +56,32 @@
     </div>
 
 
+  
+  <!-- Accordion of all skills below the top 3 -->
+  <div class="row list-wrapper">
+    <br>
+      <accordion>
+        <accordion-group is-open="isopen">
+          <div class="progress" ng-repeat="skill in skills | orderBy:'current' | limitTo: (otherSkills - 3) | orderBy:'-current'">
+            <accordion-heading>
+              More Skills<i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': isopen, 'glyphicon-chevron-right': !isopen}"></i>
+            </accordion-heading>
+            <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%">
+              <span>Current {{skill.name}} level</span>           
+            </div>
+            <div class="progress-bar progress-bar-warning" style="width: {{(skill.desired - skill.current) * 10}}%">
+              <span>Desired {{skill.name}} level</span>
+            </div>
+          </div>
+        </accordion-group>
+      </accordion>
+    </div>
+  </div>
+
   </section>
 
   <hr>
+
   <!-- static project section with dynamic project inputs -->
   <section ng-controller="UserProfileCtrl" ng-init="uid = <?php print $elements["#account"]->uid ?>" id="projects" class="container-fluid">
     <div class="project_header common_title">
@@ -68,9 +90,9 @@
 
     <hr>
 
-    <div class="project_content row" ng-repeat="project in page.related_projects">
-      <div class="project1 col-lg-4" >
-        <h3><a href="{{project.nid}}">{{project.name}}</a></h3>
+    <div class="project_content row" >
+      <div class="project col-lg-4" ng-repeat="project in page.related_projects">
+        <h3><a href="node/{{project.nid}}">{{project.name}}</a></h3>
         <div ng-bind-html="project.avatar"></div>
       </div>
     </div>
