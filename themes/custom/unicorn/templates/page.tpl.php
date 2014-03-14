083b390ab28094ee5e2547cec5d0dd3448a96d2d
sjagr <s.jager@gmail.com>
2014-03-14 16:20:06 -0400

    Oops, wrong h1, switched with the right one

--- themes/custom/unicorn/templates/page.tpl.php
+++ themes/custom/unicorn/templates/page.tpl.php
@@ -84,7 +84,7 @@
       <?php endif; ?>
 
       <?php if (!empty($site_name)): ?>
-      <?php /* <h1 class="name"><a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></h1> */ ?>
+      <h1 class="name"><a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a></h1>
       <?php endif; ?>
 
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
@@ -140,7 +140,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+        <?php /* <h1 class="page-header"><?php print $title; ?></h1> */ ?>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
