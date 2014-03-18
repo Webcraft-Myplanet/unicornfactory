f0147dead3a2cf38a7a7125f2e20ed776f246c79
bhawkeswood <bhawkeswood@gmail.com>
2014-03-18 11:06:16 -0400

    Inidividual project pages now have alts for pictures

--- themes/custom/unicorn/templates/node/node--project_profile.tpl.php
+++ themes/custom/unicorn/templates/node/node--project_profile.tpl.php
@@ -85,7 +85,7 @@
   <section class="container-fluid">
     <div class="row">
       <form editable-form name="projectForm" onaftersave="updateProject()" oncancel="cancel()">
-        <div class="projectLogo col-xs-4"><?php print $field_avatar[0]['html']; ?></div>
+        <div class="projectLogo col-xs-4" alt="{{page.title}}"><?php print $field_avatar[0]['html']; ?></div>
 
         <div class="col-xs-8">
           <div class="project_dates">
