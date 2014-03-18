48ddd0384d7a56123c7bf0442a2096ce1088e31e
bhawkeswood <bhawkeswood@gmail.com>
2014-03-18 11:03:03 -0400

    All project listing now shows alt tags for each profile picture

--- themes/custom/unicorn/templates/block/block--uf_info--uf_projects_list.tpl.php
+++ themes/custom/unicorn/templates/block/block--uf_info--uf_projects_list.tpl.php
@@ -74,7 +74,7 @@
             <accordion-heading>
               {{project.title}}<i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-up': isopen, 'glyphicon-chevron-down': !isopen}"></i>
             </accordion-heading>
-            <div class="pull-left" ng-bind-html="project.logo"></div>
+            <div class="pull-left" ng-bind-html="project.logo" alt="{{project.title}}"></div>
             <p>{{project.status}}</p>
             <p ng-bind-html="project.description"></p>
             <p ng-show="project.startDate">{{project.startDate}} - {{project.endDate}}</p>
