85cdfa6ab9843539634627029285f4d9f803fbe8
sjagr <s.jager@gmail.com>
2014-03-19 17:46:43 -0400

    Created average/composite skills list for Team Profile

--- themes/custom/unicorn/templates/node/node--team_profile.tpl.php
+++ themes/custom/unicorn/templates/node/node--team_profile.tpl.php
@@ -123,17 +123,83 @@
     <br>
     <hr>
 
-    <div class="row">
-      <div class="projectSkills col-xs-6">
-        <h3>Skills: </h3>
-        <p ng-show="page.field_skills.und.length == 0">There are no skills associated with this team</p>
-        <ul class="row" ng-show="page.field_skills.und.length > 0">
-          <li ng-repeat="skill in page.field_skills.und">
-            {{skill}}
-          </li>
+<!-- dynamic skills section with accordion fold -->
+  <section id="skills" class="container-fluid">
+    <!-- static header -->
+    <div class="row common_title">
+      <div id="skills_header" class="row">
+        <h2 class="col-xs-8">Skills</h2>
+        <ul class="col-xs-4" ng-show="page.skills.0.name">
+          <!-- This is currently hard-coded, obviously we'd like to set these dynamically according to skill levels -->
+          <li class="small" ng-repeat="skill in page.skills | orderBy: '-current'| limitTo: 1">My highest level skill is: {{skill.name}}</li>
+          <li class="small" ng-repeat="skill in page.skills | orderBy: '-desired'| limitTo: 1">My most desireable skill is: {{skill.name}}</li>
         </ul>
       </div>
-      
+    </div>
+
+    <hr>
+
+    <!-- Skills Legend -->
+    <div class="row">
+      <div id="skill_legend">
+        <h3 class="skill_name col-sm-2">Skill Guide</h3>
+        <div class="sr-only">
+          <ul>
+            <li>Level 1: Little to no knowledge</li>
+            <li>Level 2: Beginner Skill Level</li>
+            <li>Level 3: Limited Knowledge</li>
+            <li>Level 4: Competent User</li>
+            <li>Level 5: Team Contributor</li>
+            <li>Level 6: Team Leader</li>
+            <li>Level 7: Myplanet Contributor</li>
+            <li>Level 8: Myplanet Leader</li>
+            <li>Level 9: Industry Contributor</li>
+            <li>Level 10: Industry Leader</li>
+          </ul>
+        </div>
+        <div class="progress" id="legend">
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Little to no knowledge" popover-trigger="mouseenter"><div class="glyphicon glyphicon-trash"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Beginner Skill Level" popover-trigger="mouseenter"><div class="glyphicon glyphicon-thumbs-down"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Limited Knowledge" popover-trigger="mouseenter"><div class="glyphicon glyphicon-bullhorn"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Competent User" popover-trigger="mouseenter"><div class="glyphicon glyphicon-paperclip"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Team Contributor" popover-trigger="mouseenter"><div class="glyphicon glyphicon-bell"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Team Leader" popover-trigger="mouseenter"><div class="glyphicon glyphicon-pushpin"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Myplanet Contributor" popover-trigger="mouseenter"><div class="glyphicon glyphicon-tree-conifer"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Myplanet Leader" popover-trigger="mouseenter"><div class="glyphicon glyphicon-tower"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Industry Contributor" popover-trigger="mouseenter"><div class="glyphicon glyphicon-star"></div></div>
+          <div class="progress-bar progress-bar-primary" style="width: 10%" popover="Industry Leader (El Jefe!)" popover-trigger="mouseenter"><div class="glyphicon glyphicon-fire"></div></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- skills -->
+    <div class="row">
+      <p ng-show="!page.skills_composite.0.name">This user has no skills... yet!</p>
+      <div id="top3" class="row" ng-if="page.skills_composite.0.name !== ''" ng-repeat="skill in page.skills_composite | orderBy: '-current' | limitTo: 3">
+        <h3 class="skill_name col-xs-2">{{skill.name}}</h3>
+          <div class="progress col-xs-10">
+            <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%" popover="Current Skill Level: {{skill.current}}" popover-trigger="mouseenter"></div>
+            <div class="progress-bar progress-bar-warning" style="width: {{(skill.desired - skill.current) * 10}}%" popover="Desired Skill Level: {{skill.desired}}" popover-trigger="mouseenter"></div>
+          </div>
+      </div>
+    </div>
+    <div ng-show="page.skills_composite.length > 3">
+      <accordion>
+        <accordion-group is-open="isopen">
+            <accordion-heading>
+              More Skills<i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': isopen, 'glyphicon-chevron-right': !isopen}"></i>
+            </accordion-heading>
+            <div class="low_skills_composite" ng-repeat="skill in page.skills_composite | orderBy:'current' | limitTo: (page.skills_composite.length -3) | orderBy:'-current'">
+              <div class="row col-xs-12">
+              <h3 class="skill_name col-xs-2">{{skill.name}}</h3>
+                <div class="progress col-xs-10">
+                  <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%"></div>
+                  <div class="progress-bar progress-bar-warning" style="width: {{(skill.desired - skill.current) * 10}}%"></div>
+                </div>
+              </div>
+            </div>
+        </accordion-group>
+      </accordion>
     </div>
 
   </section>
