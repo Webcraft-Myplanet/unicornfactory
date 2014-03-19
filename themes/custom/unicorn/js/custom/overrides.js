aca59b14af35122a366b4051c799f6e77c840a3d
bhawkeswood <bhawkeswood@gmail.com>
2014-03-19 11:41:57 -0400

    Removed extraneous changes in override.js file

--- themes/custom/unicorn/js/custom/overrides.js
+++ themes/custom/unicorn/js/custom/overrides.js
@@ -13,19 +13,3 @@ Drupal.behaviors.bootstrapTooltips = {
       }
     }
   };
-
-// Bootstrap Popover override for the contrib Bootstrap.js file
-// POPOVER override
-
-Drupal.behaviors.bootstrapPopovers = {
-    attach: function (context, settings) {
-      if (settings.bootstrap && settings.bootstrap.popoverEnabled) {
-        var elements = $(context).find('[data-toggle="popover"]').toArray();
-        for (var i = 0; i < elements.length; i++) {
-          var $element = $(elements[i]);
-          var options = $.extend(true, {}, settings.bootstrap.popoverOptions, $element.data());
-          $element.popover({container: 'body'});
-        }
-      }
-    }
-  };
