a00b385e7f5110b3cd01a89cee76c328b4a59b24
bhawkeswood <bhawkeswood@gmail.com>
2014-03-19 10:28:44 -0400

    Adding code for popover override to top placement

--- themes/custom/unicorn/js/custom/overrides.js
+++ themes/custom/unicorn/js/custom/overrides.js
@@ -1,5 +1,5 @@
 // Bootstrap Tooltip override for the contrib Bootstrap.js file
-// SPECIFICALLY, this overrides the placement of the tooltips
+// Tooltop override
 
 Drupal.behaviors.bootstrapTooltips = {
     attach: function (context, settings) {
@@ -9,6 +9,22 @@ Drupal.behaviors.bootstrapTooltips = {
           var $element = $(elements[i]);
           var options = $.extend(true, {}, settings.bootstrap.tooltipOptions, $element.data());
           $element.tooltip({placement: 'top'});
+        }
+      }
+    }
+  };
+
+// Bootstrap Popover override for the contrib Bootstrap.js file
+// POPOVER override
+
+Drupal.behaviors.bootstrapPopovers = {
+    attach: function (context, settings) {
+      if (settings.bootstrap && settings.bootstrap.popoverEnabled) {
+        var elements = $(context).find('[data-toggle="popover"]').toArray();
+        for (var i = 0; i < elements.length; i++) {
+          var $element = $(elements[i]);
+          var options = $.extend(true, {}, settings.bootstrap.popoverOptions, $element.data());
+          $element.popover({placement: 'top'});
         }
       }
     }
