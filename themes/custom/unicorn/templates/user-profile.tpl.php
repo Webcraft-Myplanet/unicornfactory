dd94470bdbb55758d06afebca0c5d88951114d6c
bhawkeswood <bhawkeswood@gmail.com>
2014-03-19 11:43:53 -0400

    Accordion  section is now also using popovers to display levels

--- themes/custom/unicorn/templates/user-profile.tpl.php
+++ themes/custom/unicorn/templates/user-profile.tpl.php
@@ -118,9 +118,9 @@
           <div class="low_skills" ng-repeat="skill in page.skills | orderBy:'current' | limitTo: (page.skills.length -3) | orderBy:'-current'">
             <div class="row col-xs-12">
             <h3 class="skill_name col-xs-2">{{skill.name}}</h3>
-              <div class="progress col-xs-10">
-                <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%"></div>
-                <div class="progress-bar progress-bar-warning" style="width: {{(skill.desired - skill.current) * 10}}%"></div>
+              <div class="progress col-xs-10" id="base_bar">
+                <div class="progress-bar progress-bar-success" style="width: {{skill.current * 10}}%" popover="Current Skill Level: {{skill.current}}" popover-trigger="mouseenter"></div>
+                <div class="progress-bar progress-bar-warning" style="width: {{(skill.desired - skill.current) * 10}}%" popover="Desired Skill Level: {{skill.desired}}" popover-trigger="mouseenter"></div>
               </div>
             </div>
           </div>
