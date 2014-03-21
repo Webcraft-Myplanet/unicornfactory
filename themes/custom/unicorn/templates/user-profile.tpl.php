94bc76920328385acd3ad389938227fc51e17f4f
bhawkeswood <bhawkeswood@gmail.com>
2014-03-21 15:09:19 -0400

    Removed some extraneous IDs, moved divs around to unify formatting

--- themes/custom/unicorn/templates/user-profile.tpl.php
+++ themes/custom/unicorn/templates/user-profile.tpl.php
@@ -101,10 +101,10 @@
       </div>
     </div>
 
+    <p ng-show="!page.skills.0.name">This user has no skills... yet!</p>
     <!-- skills -->
-    <div class="row">
-      <p ng-show="!page.skills.0.name">This user has no skills... yet!</p>
-      <div id="top3" class="row" ng-if="page.skills.0.name !== ''" ng-repeat="skill in page.skills | orderBy: '-current' | limitTo: 3">
+    <div ng-if="page.skills.0.name !== ''" ng-repeat="skill in page.skills | orderBy: '-current' | limitTo: 3">
+      <div class="row col-xs-12">
         <h3 class="skill_name col-xs-2">{{skill.name}}</h3>
           <div class="progress col-xs-10" id="base_bar">
             <div class="progress-bar pbcurrent" style="width: {{skill.current * 10}}%" popover="Current Skill Level: {{skill.current}}" popover-trigger="mouseenter"></div>
