0d0a0f107b4aedfe65df304a6983ec5d710a8ff2
bhawkeswood <bhawkeswood@gmail.com>
2014-03-25 11:05:40 -0400

    Removed offset biz-nass

--- themes/custom/unicorn/templates/user-profile.tpl.php
+++ themes/custom/unicorn/templates/user-profile.tpl.php
@@ -42,7 +42,7 @@
      <!-- Teams -->
      <hr>
      <div class="row">
-      <h2 class="col-xs-4 offset-4">Teams</h2><br>
+      <h2 class="col-xs-4">Teams</h2><br>
       <p ng-show="page.related_teams.length == 0">This user is not a member of any teams... yet!</p>
       <div class="col-xs-10" ng-repeat="team in page.related_teams">
         <button type="button" class="btn btn-default"><a href="/node/{{team.nid}}">{{team.name}}</a></button><br>
