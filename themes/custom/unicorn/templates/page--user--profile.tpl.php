73d704a66c289453e4dfb0608806c345e6cd3d12
sjagr <s.jager@gmail.com>
2014-03-27 12:47:44 -0400

    Moved more stuff

--- themes/custom/unicorn/templates/page--user--profile.tpl.php
+++ themes/custom/unicorn/templates/page--user--profile.tpl.php
@@ -131,18 +131,6 @@
       </aside>  <!-- /#sidebar-first -->
     <?php endif; ?>
       <section<?php print $content_column_class; ?> ng-controller="UserProfileCtrl" ng-init="uid = <?php print $page['content']['system_main']["#account"]->uid ?> ">
-        <?php if (!empty($page['highlighted'])): ?>
-          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-        <?php endif; ?>
-        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-        <a id="main-content"></a>
-        <?php print $messages; ?>
-        <?php if (!empty($page['help'])): ?>
-          <?php print render($page['help']); ?>
-        <?php endif; ?>
-        <?php if (!empty($action_links)): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
         <div class="col-xs-2 left_sidebar">
           <?php print render($title_prefix); ?>
           <?php if (!empty($title)): ?>
@@ -199,6 +187,18 @@
             </div>
           </section>
         </div>
+        <?php if (!empty($page['highlighted'])): ?>
+          <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+        <?php endif; ?>
+        <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+        <a id="main-content"></a>
+        <?php print $messages; ?>
+        <?php if (!empty($page['help'])): ?>
+          <?php print render($page['help']); ?>
+        <?php endif; ?>
+        <?php if (!empty($action_links)): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
         <?php if (!empty($tabs)): ?>
           <?php print render($tabs); ?>
         <?php endif; ?>
