b0fd9e13bd0dc37f04a0dd26b93bd4f1410cfceb
sebsebseb123 <sebastian.lesch@gmail.com>
2014-07-08 16:17:49 -0400

    Taking a break.

--- themes/custom/unicorn_reborn/template.php
+++ themes/custom/unicorn_reborn/template.php
@@ -13,6 +13,26 @@ function unicorn_reborn_preprocess_html(&$vars) {
       'content' =>  'width=device-width, initial-scale=1',
     )
   );
-  
+
   drupal_add_html_head($meta_device_scale, 'meta_device_scale');
+}
+
+/**
+ * Implements hook_preprocess_node().
+ */
+function unicorn_reborn_preprocess_node(&$vars) {
+  switch($vars['type']) {
+    case 'kicklow' :
+dsm($vars);
+      // Make the date more readable.
+      $vars['date'] = date('F jS, Y', $vars['created']);
+
+      // Make a "Project Type" variable.
+      $vars['project_type'] = $vars['field_type'][0]['value'];
+
+      // Make a logo variable.
+      $image_url = image_style_url('thumbnail', $vars['field_kl_logo'][0]['uri']);
+      $vars['project_logo'] = '<img src="' . $image_url . '" />';
+      break;
+  }
 }
