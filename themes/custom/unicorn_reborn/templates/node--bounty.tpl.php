c00deb649631f221065c5a505448ba457d75bd7b
PatriciaMader <purple_blueish_elephantmonkey@hotmail.com>
2014-07-09 11:02:19 -0400

    added divs for a bounty

--- themes/custom/unicorn_reborn/templates/node--bounty.tpl.php
+++ themes/custom/unicorn_reborn/templates/node--bounty.tpl.php
@@ -81,6 +81,16 @@
  */
 
 ?>
+<div class='bounty'>
+  <h3 class='bountyTitle'></h3>
+  <div class='progress'></div>
+  <p class='date'></p>
+  <p class='description'></p>
+  <div class='apply'></div>
+</div>
+
+
+
   <?php print $test; ?>
   <p>Here's a string!!</p>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
