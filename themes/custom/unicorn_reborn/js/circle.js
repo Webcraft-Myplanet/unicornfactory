06dc5aab339750872173f1d35609117f4efbb529
katedee <katedrwecka@gmail.com>
2014-08-05 14:45:47 -0400

    fixed indenting

--- themes/custom/unicorn_reborn/js/circle.js
+++ themes/custom/unicorn_reborn/js/circle.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function ($) {
       highlight: "#A9B6C9",
       label: "to be done"
     },
-]
+  ]
   var kicklowData = [
     {
       value:  (Drupal.settings.tasks.kicklow_percent_complete),
@@ -27,8 +27,8 @@ jQuery(document).ready(function ($) {
       highlight: "#A9B6C9",
       label: "to be done"
     }
-]
-var bountyData = [
+  ]
+  var bountyData = [
     {
       value:  (Drupal.settings.tasks.bounty_percent_complete),
       color: "#F05a28",
@@ -41,40 +41,22 @@ var bountyData = [
       highlight: "#A9B6C9",
       label: "to be done"
     }
-]
-Chart.defaults.global.responsive = true;
+  ]
+  Chart.defaults.global.responsive = true;
 
-// Boolean - Whether to show labels on the scale
-Chart.defaults.global.showTooltips = false;
+  // Boolean - Whether to show labels on the scale
+  Chart.defaults.global.showTooltips = false;
 
-    // Interpolated JS string - can access value
-Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%> <%}%>";
+  // Interpolated JS string - can access value
+  Chart.defaults.global.tooltipTemplate = "<%if (label){%><%=label%> <%}%>";
 
-// Get context with jQuery - using jQuery's .get() method.
-var ctx = $("#totalProgressChart").get(0).getContext("2d");
-var ctx1 = $("#kicklowChart").get(0).getContext("2d");
-var ctx2 = $("#bountyChart").get(0).getContext("2d");
+  // Get context with jQuery - using jQuery's .get() method.
+  var ctx = $("#totalProgressChart").get(0).getContext("2d");
+  var ctx1 = $("#kicklowChart").get(0).getContext("2d");
+  var ctx2 = $("#bountyChart").get(0).getContext("2d");
 
-// This will get the first returned node in the jQuery collection.
-var myDoughnutChart = new Chart(ctx).Doughnut(totalProgressData, {
-
-    //Number - The percentage of the chart that we cut out of the middle
-    percentageInnerCutout : 60, // This is 0 for Pie charts
-
-    //Number - Amount of animation steps
-    animationSteps : 80,
-
-    //String - Animation easing effect
-    animationEasing : "easeOutExpo",
-
-    onAnimationComplete: function() {
-            ctx.font = 'bold 23px Arial';
-            ctx.textAlign= "center";
-            ctx.textBaseline = "middle";
-            ctx.fillText(totalProgressData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
-            }
-    });
-var myDoughnutChart = new Chart(ctx1).Doughnut(kicklowData, {
+  // This will get the first returned node in the jQuery collection.
+  var totalChart = new Chart(ctx).Doughnut(totalProgressData, {
 
     //Number - The percentage of the chart that we cut out of the middle
     percentageInnerCutout : 60, // This is 0 for Pie charts
@@ -86,13 +68,14 @@ var myDoughnutChart = new Chart(ctx1).Doughnut(kicklowData, {
     animationEasing : "easeOutExpo",
 
     onAnimationComplete: function() {
-            ctx1.font = 'bold 23px Arial';
-            ctx1.textAlign= "center";
-            ctx1.textBaseline = "middle";
-            ctx1.fillText(kicklowData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
-            }
-    });
-var myDoughnutChart = new Chart(ctx2).Doughnut(bountyData, {
+      ctx.font = 'bold 23px Arial';
+      ctx.textAlign= "center";
+      ctx.textBaseline = "middle";
+      ctx.fillText(totalProgressData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
+    }
+  });
+
+  var kicklowChart = new Chart(ctx1).Doughnut(kicklowData, {
 
     //Number - The percentage of the chart that we cut out of the middle
     percentageInnerCutout : 60, // This is 0 for Pie charts
@@ -104,13 +87,30 @@ var myDoughnutChart = new Chart(ctx2).Doughnut(bountyData, {
     animationEasing : "easeOutExpo",
 
     onAnimationComplete: function() {
-            ctx2.font = 'bold 23px Arial';
-            ctx2.textAlign= "center";
-            ctx2.textBaseline = "middle";
-            ctx2.fillText(bountyData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
-            }
-    });
+      ctx1.font = 'bold 23px Arial';
+      ctx1.textAlign= "center";
+      ctx1.textBaseline = "middle";
+      ctx1.fillText(kicklowData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
+    }
+  });
 
-});
+  var bountyChart = new Chart(ctx2).Doughnut(bountyData, {
+    //Number - The percentage of the chart that we cut out of the middle
+    percentageInnerCutout : 60, // This is 0 for Pie charts
+
+    //Number - Amount of animation steps
+    animationSteps : 80,
+
+    //String - Animation easing effect
+    animationEasing : "easeOutExpo",
+
+    onAnimationComplete: function() {
+      ctx2.font = 'bold 23px Arial';
+      ctx2.textAlign= "center";
+      ctx2.textBaseline = "middle";
+      ctx2.fillText(bountyData[0].value + "%", ctx.canvas.width/2 , ctx.canvas.width/2);
+    }
+  });
+});//end of doc ready
 
 
